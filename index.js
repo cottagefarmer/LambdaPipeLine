af1805b57ddb43fa02e2f2781542217f62764bf5
Patrick <patrick@runthisproject.com>
2018-05-13 19:50:12 -0400

    changed version

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var time = require('time');
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date(); 
     currentTime.setTimezone("America/Los_Angeles");
-    //comment
     callback(null, {
         statusCode: '200',
         body: 'The time in Los Angeles is: ' + currentTime.toString(),
